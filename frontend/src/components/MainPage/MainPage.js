d09ef0fc3590d5c48b7742ed59e31e9879a165a7
Michelle Li <rphmichelle@gmail.com>
2023-08-06 17:52:25 -0400

    create navbar and main page

---
+++ frontend/src/components/MainPage/MainPage.js
@@ -0,0 +1,13 @@
+
+function MainPage() {
+    return (
+      <>
+        <p>A Twitter Clone</p>
+        <footer>
+          Copyright &copy; 2022 Chirper
+        </footer>
+      </>
+    );
+  }
+  
+  export default MainPage;
